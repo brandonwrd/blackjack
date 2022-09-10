a66aeaa60a58cc9940537bd2a954d832873b06f6
brandonwrd <wardesque@gmail.com>
2022-09-10 02:31:04 -0400

    The Game finished

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ How I want the game to be structured: <--- add to this if you more comes to mind
 - random 2 cards revealed for player 2 or computer
 - the rest of the deck faced down (dealer cards)
 - have the deck choose random cards to give players
-- ace card = 1 or 11
+- ace card = 11
 - a button to "hit" or "stay" when prompted for each player
 - a score of some sort next to or around the player's cards
 - want the cards and buttons to be centered on the screen
@@ -17,23 +17,25 @@ How I want the game to be structured: <--- add to this if you more comes to mind
 
 Code to implement:
 
-- a shuffling function (WORK IN PROGRESS)
+- a shuffling function (DONE)
 - a function for the whole deck of cards (DONE)
-- players (WORK IN PROGRESS)
-- a function for the buttons and actions to match (WORK IN PROGRESS)
+- players (DONE)
+- a function for the buttons and actions to match (DONE)
 - card images in HTML (DONE - in cards folder)
-- figure out a way to center everything (DONE/WORK IN PROGRESS)- still trying to figure out how to position where the score is going to be elsewhere)
+- figure out a way to center everything (DONE)
 
 What I want to figure out:
 
-- how to add text to the side of the images
-- how to move the dealer stack wherever i want
-- a start game function
-- whether or not you want the game to be between player 1 and player two or player 1 vs computer
+- how to add text to the side of the images (not needed)
+- how to move the dealer stack wherever i want (not needed - decks centered)
+- a start game function (DONE)
+- whether or not you want the game to be between player 1 and player two or player 1 vs computer (decided to do player vs computer)
+- how to make the restart button refresh the page and give new cards (DONE)
 
-Source help (while using only concepts learned from class):
+Source help (entered in some comments within js file as well):
 - https://stackoverflow.com/questions/58473137/how-to-change-add-image-via-js-dom-in-blackjack-game
-- https://stackoverflow.com/questions/27029157/how-to-center-align-script#:~:text=For%20text%20centering%20use%3A,using%20styles%20applied%20by%20javascript.
+- https://stackoverflow.com/questions/27029157/how-to-center-align-script#:~:text=For%20text%20centering%20use%3Ausing%20styles%20applied%20by%20javascript.
 - https://github.com/ImKennyYip/black-jack/tree/master/cards (for card images)
 - https://stackoverflow.com/questions/17219757/how-to-place-text-and-an-image-next-to-each-other-in-html
 - https://www.w3schools.com/jsref/jsref_isnan.asp
+- https://developer.mozilla.org/en-US/docs/Web/API/Element/append (grabbing card values with from card images)
--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 // SCORE OF PLAYERS AND RULESETS
 playerSco = 0
-computerSco = 0
+dealerSco = 0
 
 playerAces = 0
-computerAces = 0
+dealerAces = 0
 
-let playerOneHit = true;
-let card;
+let playerCanHit = true;
 let dealerDeck;
 let deck;
 
-// DECK OF CARDS
+// DECK OF CARDS (DONE)
 function fullDeck() {
         let cardValues = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
-        let suits = ["Spades", "Hearts", "Clubs", "Diamonds"];
+        let suits = ["S", "H", "C", "D"];
         deck = [];
 
         for (let i = 0; i < cardValues.length; i++) {
             for (let x = 0; x < suits.length; x++) {
-                deck.push(cardValues[i] + " of " + suits[x]);
+                deck.push(cardValues[i] + "-" + suits[x]);
             }
         }
         // console.log(deck); // ex: "2 of Diamonds, 6 of Clubs, 9 of Hearts, etc."
@@ -26,11 +25,11 @@ function fullDeck() {
 
 // GRABBING CARD VALUES
 function getValue(dealerDeck) {
-    let findCard = dealerDeck.split("of");
+    let findCard = dealerDeck.split("-"); // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
     let value = findCard[0];
     
     if (isNaN(value)) {
-        if (value === "A") {
+        if (value == "A") {
             return 11;
         }
         return 10; // if not 11 it is a K,Q,J 
@@ -38,7 +37,23 @@ function getValue(dealerDeck) {
     return parseInt(value); // if not KQJA it will find a random number within the deck
 
 }
-// SHUFFLE CARDS FUNCTION 
+// FUNCTION TO CHECK ACES
+function checkForAces(dealerDeck) {
+    if(dealerDeck[0] == "A") {
+        return 1;
+    }
+    return 0;
+}
+
+// FUNCTION TO KEEP TRACK OF ACES
+function reduceAce(playerSco, playerAces) {
+    while (playerSco > 21 && playerAces > 0) {
+        playerSco -= 10; 
+        playerAces -= 1;
+    }
+    return playerSco;
+}
+// SHUFFLE CARDS FUNCTION
 function shuffleCards() {
     for (let i = 0; i < deck.length; i++) {
         let randomCards = Math.floor(Math.random() * deck.length) // should select a random card within deck (52) to supply
@@ -50,48 +65,94 @@ function shuffleCards() {
 
 // START GAME FUNCTION
 function playBlackjack() {
-    let cardImages = document.createElement("img");
     dealerDeck = deck.pop();
-    computerSco += getValue(card)
-    cardImages.src = "./cards" + card + ".png"
+    dealerSco += getValue(dealerDeck);
+    dealerAces += checkForAces(dealerDeck);
+    // console.log(dealerDeck); // this finds the whole card
+    // console.log(dealerSco); // this separates the point value from suit
+    while (dealerSco < 17) {
+        let cardImages = document.createElement("img");
+        let card = deck.pop();
+        cardImages.src = "./cards/" + card + ".png"
+        dealerSco += getValue(card); // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Addition_assignment
+        dealerAces += checkForAces(card);
+        document.getElementById("dealerCards").append(cardImages);
+    }
+    console.log(dealerSco);
+
+    for (let i = 0; i < 2; i++) {
+        let cardImages = document.createElement("img");
+        let card = deck.pop();
+        cardImages.src = "./cards/" + card + ".png";
+        playerSco += getValue(card);
+        playerAces += checkForAces(card);
+        document.getElementById("playerCards").append(cardImages);
+    }
+    console.log(playerSco);
+    document.getElementById("hit-button").addEventListener("click", hit);
+    document.getElementById("stay-button").addEventListener("click", stay);
+    document.getElementById("restart-button").addEventListener("click", function(){ // https://teamtreehouse.com/community/any-one-know-how-to-make-a-restart-button 
+        window.location.reload();
+        return false;
+      }); // this refreshes the page
 
 
 }
-// console.log();
 
-// BUTTONS (Hit, Stay, and Restart AND WHAT YOU WANT TO APPEAR ON THE SCREEN TO SHOW OUTCOMES)
+// BUTTONS (HIT, STAY, and RESTART AND WHAT YOU WANT TO APPEAR ON THE SCREEN TO SHOW OUTCOMES)
 function hit() {  // if score of player one or two is less than 21, but wants to try to get the highest score without going over 21, then give them the option to hit
-    document.getElementById("hit-button").addEventListener("click")
+    if(!playerCanHit) {
+        return;
+    }
+    let cardImages = document.createElement("img");
+        let card = deck.pop();
+        cardImages.src = "./cards/" + card + ".png"
+        playerSco += getValue(card);
+        playerAces += checkForAces(card);
+        document.getElementById("playerCards").append(cardImages);
+
+    if(reduceAce(playerSco, playerAces) > 21) {
+        playerCanHit = false
+    }
     
 }
 
 function stay() { // if score of player one or two is less than 21 and they don't want to go over 21, then give them the option to stay at that number
-    
+    dealerSco = reduceAce(dealerSco, dealerAces);
+    playerSco = reduceAce(playerSco, playerAces);
+
+    playerCanHit = false
+    document.getElementById("hiddenCards").src = "./cards/" + dealerDeck + ".png"
+
+    let outcome = ""
     if (playerSco > 21) {
-        console.log("Computer Wins!");
+        outcome = "You Lose!";
     } 
     else if (dealerSco > 21) {
-        console.log("Player 1 Wins!");
+        outcome = "You Win!";
     } 
     else if (playerSco === dealerSco) {
-        console.log("Tie!");
+        outcome = "It's a Tie!";
     } 
     else if (playerSco > dealerSco) {
-        console.log("Player 1 Wins");
+        outcome = "You Win!";
     } 
     else if (playerSco < dealerSco) {
-        console.log("Computer Wins!");
+        outcome = "You Lose!";
     }
-    document.getElementById("stay-button").addEventListener("click")
+
+    document.getElementById("dealerScore").innerHTML = dealerSco;
+    document.getElementById("playerScore").innerHTML = playerSco;
+    document.getElementById("finalOutcome").innerHTML = outcome;
+    
     
 }
 
 function restart() { // if player one or player two wins, press restart button to go again
-    document.getElementById("restart-button").addEventListener("click")
-
+    window.onload();
 }
 
 
 fullDeck();
 shuffleCards();
-// // playBlackjack();
+playBlackjack();
